22a1868afd9253dcbb5c5be9408af28729bbcf59
Victoria Holland <JavaDevVictoria@users.noreply.github.com>
2019-03-27 09:47:57 +0000

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,27 @@
+public class fibonacci {
+
+     public static void main(String []args) {
+		int lengthOfSequence = 10;
+		for (int i = 0; i < lengthOfSequence; i++) {
+			if (i == lengthOfSequence - 1) {
+				System.out.print(fibonacci(i));
+			}
+			else {
+				System.out.print(fibonacci(i) + ",");
+			}
+		}
+     }
+     
+     public static int fibonacci(int n)  {
+        if(n == 0) {
+            return 0;
+        }
+        else if(n == 1) {
+            return 1;
+        }
+        else {
+			int fibTerm = fibonacci(n - 1) + fibonacci(n - 2);
+            return fibTerm;
+        }
+    }
+}
