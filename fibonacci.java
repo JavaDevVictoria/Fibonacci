6b79a0285ee4f08b671cb881c44c8973e0b53e1b
Victoria Holland <JavaDevVictoria@users.noreply.github.com>
2019-03-27 09:51:15 +0000

    Update fibonacci.java
    Tidied formatting

--- fibonacci.java
+++ fibonacci.java
@@ -1,6 +1,6 @@
 public class fibonacci {
 
-     public static void main(String []args) {
+	public static void main(String []args) {
 		int lengthOfSequence = 10;
 		for (int i = 0; i < lengthOfSequence; i++) {
 			if (i == lengthOfSequence - 1) {
@@ -10,18 +10,18 @@ public class fibonacci {
 				System.out.print(fibonacci(i) + ",");
 			}
 		}
-     }
-     
-     public static int fibonacci(int n)  {
-        if(n == 0) {
-            return 0;
-        }
-        else if(n == 1) {
-            return 1;
-        }
-        else {
-			int fibTerm = fibonacci(n - 1) + fibonacci(n - 2);
-            return fibTerm;
-        }
     }
+     
+	public static int fibonacci(int n)  {
+		if(n == 0) {
+			return 0;
+		}
+		else if(n == 1) {
+			return 1;
+		}
+		else {
+			int fibTerm = fibonacci(n - 1) + fibonacci(n - 2);
+			return fibTerm;
+		}
+	}
 }
